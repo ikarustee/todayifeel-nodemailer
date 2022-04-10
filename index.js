4eace689cc228f0357d13db5eb3bd40b381be968
Tanja Karius <tanja.karius@googlemail.com>
2022-04-10 20:02:04 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+const app = express();
+const cors = require("cors")
+require("dotenv").config();
+
+// middlware
+app.use(express.json())
+app.use(cors())
+
+let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      type: "OAuth2",
+      user: process.env.EMAIL,
+      pass: process.env.WORD,
+      clientId: process.env.OAUTH_CLIENTID,
+      clientSecret: process.env.OAUTH_CLIENT_SECRET,
+      refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+    },
+   });
+
+transporter.verify((err, success) => {
+err
+    ? console.log(err)
+    : console.log(`=== Server is ready to take messages: ${success} ===`);
+});
+
+app.post("/send", function (req, res) {
+    console.log(req.body)
+    // let mailOptions = {
+    //     from: "test@gmail.com",
+    //     to: process.env.EMAIL,
+    //     subject: "Nodemailer API",
+    //     text: "Hi from your nodemailer API",
+    //   };
+    let mailOptions = {
+      from: `${req.body.mailerState.email}`,
+      to: process.env.EMAIL,
+      subject: `Today I Feel – article was reported ${req.body.mailerState.article}`,
+      html: `<h4>Reason: ${req.body.mailerState.value}</h4> <p>Comment: ${req.body.mailerState.message}</p>`,
+    };
+   
+    transporter.sendMail(mailOptions, function (err, data) {
+      if (err) {
+        res.json({
+            status: "Fail",
+        })
+      } else {
+        console.log("== Message sent ==");
+        res.json({ status: "success" });
+      }
+    });
+   });
+
+
+const port = 3001;
+app.listen(port, () => {
+ console.log(`Server is running on port: ${port}`);
+});
